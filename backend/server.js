26277d60c3351a7be4789c54cc8f14296973b4fd
KunalGupta3110 <kunalgupta362007@gmail.com>
2026-04-24 19:19:23 +0530

    added node-fetch

--- backend/server.js
+++ backend/server.js
@@ -128,6 +128,8 @@ Output only final document.
   }
 });
 // ================= SERVER =================
-app.listen(5000, () => {
-  console.log("🚀 Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
